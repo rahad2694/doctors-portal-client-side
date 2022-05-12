fa8d49ac86474be1f93b08bfe3b7ac62a190148c
rahad2694 <rahad.haw@gmail.com>
2022-05-13 03:39:36 +0600

    Banner-1 and Service added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './pages/Home/About/About';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import NotFound from './pages/NotFound/NotFound';
 import NavBar from './pages/Shared/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <NavBar></NavBar>
       {/* <button className='btn btn-primary'>Hi</button> */}
 
       <Routes>
-        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Banner from './Banner';
+import Services from './Services';
 
 const Home = () => {
     return (
         <div>
-            This is Home
+            <Banner></Banner>
+            <div className='lg:my-32 my-18'>
+                <h1 className='mt-6 text-center text-primary font-bold'>OUR SERVICES</h1>
+                <h1 className='text-center text-2xl mt-2 mb-8'>Services we provide</h1>
+            </div>
+            <Services></Services>
         </div>
     );
 };
