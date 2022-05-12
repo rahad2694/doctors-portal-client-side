f5732f39fc1156741931dc02c2465d05ebf1838c
rahad2694 <rahad.haw@gmail.com>
2022-05-12 13:48:09 +0600

    Doctors portal Project initiated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/Home/About/About';
+import Home from './pages/Home/Home';
+import NavBar from './pages/Shared/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      {/* <button className='btn btn-primary'>Hi</button> */}
+
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
