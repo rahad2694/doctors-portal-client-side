265b79305534cdf1e014fc7283ba94dcba539624
rahad2694 <rahad.haw@gmail.com>
2022-05-16 10:24:00 +0600

    Auth reload issue solved

--- src/pages/Shared/RequireAuth.js
+++ src/pages/Shared/RequireAuth.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
+import LoadingSpinner from './LoadingSpinner';
 
 const RequireAuth = ({children}) => {
   let location = useLocation();
-  const [user] = useAuthState(auth);
+  const [user,loading] = useAuthState(auth);
+
+  if(loading){
+      return <LoadingSpinner></LoadingSpinner>
+  }
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
